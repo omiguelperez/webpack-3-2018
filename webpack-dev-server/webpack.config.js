15eec52a934fe160eb1627cad9fbba69903229e4
Oscar Pérez <mr.omiguelperez@gmail.com>
2019-07-11 06:27:46 -0500

    webpack dev server

---
+++ webpack-dev-server/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports ={
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname),
+    compress: true,
+    port: 8080,
+    open: true
+  }
+}
